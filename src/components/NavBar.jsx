14fe67739d1bb8ec45d2cdf61f48447636b6c247
Augusto Lujan Garcia <lujangarcia@fie.undef.edu.ar>
2023-03-06 16:50:17 -0300

    Add - chakra, style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CartWidget from './CartWidget'
-import { Menu, MenuButton, MenuList, MenuItem, Image } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuList, MenuItem, Image, Box, Flex, Spacer, Heading } from '@chakra-ui/react'
 import componentes from '../images/componentes.webp'
 import escritorio from '../images/escritorio.webp'
 import gabinete from '../images/gabinete.webp'
@@ -9,66 +9,78 @@ import silla from '../images/silla.webp'
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>E-Commerce</h1>
-            <Menu>
-                <MenuButton>
-                    Categorías
-                </MenuButton>
-                <MenuList>
-                    <MenuItem minH='48px'>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src={componentes}
-                            alt='Componentes'
-                            mr='12px'
-                        />
-                        <span>Componentes</span>
-                    </MenuItem>
-                    <MenuItem minH='40px'>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src={escritorio}
-                            alt='Escritorios'
-                            mr='12px'
-                        />
-                        <span>Escritorios</span>
-                    </MenuItem>
-                    <MenuItem minH='40px'>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src={gabinete}
-                            alt='Gabinetes'
-                            mr='12px'
-                        />
-                        <span>Gabinetes</span>
-                    </MenuItem>
-                    <MenuItem minH='40px'>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src={perifericos}
-                            alt='Periféricos'
-                            mr='12px'
-                        />
-                        <span>Periféricos</span>
-                    </MenuItem>
-                    <MenuItem minH='40px'>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src={silla}
-                            alt='Sillas'
-                            mr='12px'
-                        />
-                        <span>Sillas</span>
-                    </MenuItem>
-                </MenuList>
-            </Menu>
-            <CartWidget />
+        <div className='nav'>
+            <Box>
+                <Flex>
+                    <Box w='70px' h='10' p='4'>
+                        <Menu>
+                            <MenuButton>
+                                Categorías
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem minH='48px'>
+                                    <Image
+                                        boxSize='2rem'
+                                        borderRadius='full'
+                                        src={componentes}
+                                        alt='Componentes'
+                                        mr='12px'
+                                    />
+                                    <span>Componentes</span>
+                                </MenuItem>
+                                <MenuItem minH='40px'>
+                                    <Image
+                                        boxSize='2rem'
+                                        borderRadius='full'
+                                        src={escritorio}
+                                        alt='Escritorios'
+                                        mr='12px'
+                                    />
+                                    <span>Escritorios</span>
+                                </MenuItem>
+                                <MenuItem minH='40px'>
+                                    <Image
+                                        boxSize='2rem'
+                                        borderRadius='full'
+                                        src={gabinete}
+                                        alt='Gabinetes'
+                                        mr='12px'
+                                    />
+                                    <span>Gabinetes</span>
+                                </MenuItem>
+                                <MenuItem minH='40px'>
+                                    <Image
+                                        boxSize='2rem'
+                                        borderRadius='full'
+                                        src={perifericos}
+                                        alt='Periféricos'
+                                        mr='12px'
+                                    />
+                                    <span>Periféricos</span>
+                                </MenuItem>
+                                <MenuItem minH='40px'>
+                                    <Image
+                                        boxSize='2rem'
+                                        borderRadius='full'
+                                        src={silla}
+                                        alt='Sillas'
+                                        mr='12px'
+                                    />
+                                    <span>Sillas</span>
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+                    <Spacer />
+                    <Box w='400px' h='10' p='4'>
+                        <Heading id='title' size='md'>Gamer Tech</Heading>
+                    </Box>
+                    <Spacer />
+                    <Box w='150px' h='10' p='4'>
+                        <CartWidget />
+                    </Box>
+                </Flex>
+            </Box>
         </div>
     )
 }
