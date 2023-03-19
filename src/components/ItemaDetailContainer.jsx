5472a0f0779a5f8d4b9a27552b9e85fcf0b002c0
Augusto Lujan Garcia <lujangarcia@fie.undef.edu.ar>
2023-03-19 16:50:07 -0300

    Add - Routes

---
+++ src/components/ItemaDetailContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemaDetailContainer = () => {
+  return (
+    <div>ItemaDetailContainer</div>
+  )
+}
+
+export default ItemaDetailContainer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import escritorio from '../images/escritorio.webp'
 import gabinete from '../images/gabinete.webp'
 import perifericos from '../images/perifericos.webp'
 import silla from '../images/silla.webp'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
@@ -14,70 +15,86 @@ const NavBar = () => {
                 <Flex>
                     <Center w='70px' h='20' ml='7'>
                         <Menu>
-                            <MenuButton>
-                                Categorías
-                            </MenuButton>
+                            <Link to={"/productos"}>
+                                <MenuButton>
+                                    Categorías
+                                </MenuButton>
+                            </Link>
                             <MenuList>
-                                <MenuItem minH='48px'>
-                                    <Image
-                                        boxSize='2rem'
-                                        borderRadius='full'
-                                        src={componentes}
-                                        alt='Componentes'
-                                        mr='12px'
-                                    />
-                                    <span>Componentes</span>
-                                </MenuItem>
-                                <MenuItem minH='40px'>
-                                    <Image
-                                        boxSize='2rem'
-                                        borderRadius='full'
-                                        src={escritorio}
-                                        alt='Escritorios'
-                                        mr='12px'
-                                    />
-                                    <span>Escritorios</span>
-                                </MenuItem>
-                                <MenuItem minH='40px'>
-                                    <Image
-                                        boxSize='2rem'
-                                        borderRadius='full'
-                                        src={gabinete}
-                                        alt='Gabinetes'
-                                        mr='12px'
-                                    />
-                                    <span>Gabinetes</span>
-                                </MenuItem>
-                                <MenuItem minH='40px'>
-                                    <Image
-                                        boxSize='2rem'
-                                        borderRadius='full'
-                                        src={perifericos}
-                                        alt='Periféricos'
-                                        mr='12px'
-                                    />
-                                    <span>Periféricos</span>
-                                </MenuItem>
-                                <MenuItem minH='40px'>
-                                    <Image
-                                        boxSize='2rem'
-                                        borderRadius='full'
-                                        src={silla}
-                                        alt='Sillas'
-                                        mr='12px'
-                                    />
-                                    <span>Sillas</span>
-                                </MenuItem>
+                                <Link to={`/categorias/${"Componentes"}`}>
+                                    <MenuItem minH='48px'>
+                                        <Image
+                                            boxSize='2rem'
+                                            borderRadius='full'
+                                            src={componentes}
+                                            alt='Componentes'
+                                            mr='12px'
+                                        />
+                                        <span>Componentes</span>
+                                    </MenuItem>
+                                </Link>
+                                <Link to={`/categorias/${"Escritorios"}`}>
+                                    <MenuItem minH='40px'>
+                                        <Image
+                                            boxSize='2rem'
+                                            borderRadius='full'
+                                            src={escritorio}
+                                            alt='Escritorios'
+                                            mr='12px'
+                                        />
+                                        <span>Escritorios</span>
+                                    </MenuItem>
+                                </Link>
+                                <Link to={`/categorias/${"Gabinetes"}`}>
+                                    <MenuItem minH='40px'>
+                                        <Image
+                                            boxSize='2rem'
+                                            borderRadius='full'
+                                            src={gabinete}
+                                            alt='Gabinetes'
+                                            mr='12px'
+                                        />
+                                        <span>Gabinetes</span>
+                                    </MenuItem>
+                                </Link>
+                                <Link to={`/categorias/${"Perifericos"}`}>
+                                    <MenuItem minH='40px'>
+                                        <Image
+                                            boxSize='2rem'
+                                            borderRadius='full'
+                                            src={perifericos}
+                                            alt='Periféricos'
+                                            mr='12px'
+                                        />
+                                        <span>Periféricos</span>
+                                    </MenuItem>
+                                </Link>
+                                <Link to={`/categorias/${"Sillas"}`}>
+                                    <MenuItem minH='40px'>
+                                        <Image
+                                            boxSize='2rem'
+                                            borderRadius='full'
+                                            src={silla}
+                                            alt='Sillas'
+                                            mr='12px'
+                                        />
+                                        <span>Sillas</span>
+                                    </MenuItem>
+                                </Link>
                             </MenuList>
                         </Menu>
                     </Center>
                     <Spacer />
                     <Center w='400px' h='20'>
-                        <Heading id='title' size='md'>Gamer Tech</Heading>
+                        <Heading id='title' size='md'>
+                            <Link to={"/"}>Gamer Tech</Link>
+                        </Heading>
                     </Center>
                     <Spacer />
                     <Center w='150px' h='20'>
-                        <CartWidget />
+                        <Link to={"/cart"}>
+                            <CartWidget />
+                        </Link>
                     </Center>
                 </Flex>
             </Box>
